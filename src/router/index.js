b2ba3360652017fce50344d52100d4d59e10798c
JavohirBozorboyev <javohirbozorboyev2002@gmail.com>
2024-08-18 23:44:09 -0700

    notfoundlayout bags

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import AppLayout from "@/layout/AppLayout.vue";
 import ProductLayout from "@/layout/ProductLayout.vue";
 import CategoryView from "@/views/CategoryView.vue";
 import NotFoundView from "@/views/NotFoundView.vue";
+import NotFoundLayout from "@/layout/NotFoundLayout.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
