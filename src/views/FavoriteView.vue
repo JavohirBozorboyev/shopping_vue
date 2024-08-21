ab88db344524ca5975f872706ebc5fc82e386935
UZB02 <muhsinbekmirzamatov3@gmail.com>
2024-08-21 22:35:14 +0500

    Favorite top nav do scrol

--- src/views/FavoriteView.vue
+++ src/views/FavoriteView.vue
@@ -1,5 +1,7 @@
 <script setup>
 import { ref } from "vue";
+import Button from "primevue/button";
+import Badge from 'primevue/badge';
 const data = ref([
   {
     id: 1234456,
@@ -53,63 +55,66 @@ const data = ref([
 </script>
 
 <template>
-  <div class="container mx-auto p-2 text-black bg-white">
-    <div class="bottom flex flex-col gap-3 p-1 px-3 w-full overflow-y-auto">
-      <div class="grid grid-cols-12 items-center gap-2">
-        <div
-          v-for="item in data"
-          class="col-span-12 sm:col-span-6 md:col-span-4 xl:col-span-3 2xl:col-span-3 p-2 flex flex-col gap-1 relative bg-gray-50 rounded-lg"
+  <div class="grid grid-cols-12 gap-2 xl:gap-3">
+      <div v-for="item in data" class="col-span-6 md:col-span-3">
+             <div
+      class="col-span-6 xl:col-span-4 2xl:col-span-3 p-2 xl:p-3 rounded-md border hover:border-black duration-300"
+    >
+      <div class="">
+        <img
+          src="https://avatars.mds.yandex.net/i?id=00782806b70a7cd694288869be75dc857225a87a-4593530-images-thumbs&n=13"
+          alt=""
+          class="w-full h-32 md:min-h-48 object-cover rounded-md"
+        />
+      </div>
+      <div class="mt-2">
+        <div class="flex justify-between items-center">
+          <p class="text-[10px] lg:text-xs text-gray-400 line-clamp-1">
+            Namangan Viloyati
+          </p>
+
+          <Badge value="Yangi" size="small" severity="secondary"></Badge>
+        </div>
+        <h1
+          class="text-xs lg:text-sm mt-2 text-slate-700 font-semibold line-clamp-2"
         >
-          <span
-            class="flex absolute right-3 bg-gray-100 p-2 rounded-sm top-3 items-center justify-end active:scale-95 duration-300"
-          >
-            <i class="pi pi-trash text-gray-400 cursor-pointer"></i
-          ></span>
-          <div class="top w-full flex flex-wrap">
-            <img
-              class="w-full h-[250px] object-cover"
-              src="https://avatars.mds.yandex.net/i?id=fafaa03adad3294c6556ec9c5a753b693307739a-9146954-images-thumbs&n=13"
-              alt=""
-            />
+          Sarlavha
+        </h1>
+        <h1
+          class="text-slate-700 font-semibold text-sm mt-2 flex items-center gap-2"
+        >
+          <i class="pi pi-money-bill text-gray-400"></i>
+          300 dollor
+        </h1>
+        <div class="mt-4 flex justify-between gap-2">
+          <div class="flex gap-2">
+            <Button
+              class=""
+              icon="pi pi-shopping-cart "
+              size="small"
+              severity="contrast"
+            ></Button>
+
+            <Button
+              class=""
+              icon="pi pi-heart"
+              size="small"
+              outlined
+              severity="contrast"
+            ></Button>
           </div>
-          <div class="bottom flex flex-col gap-2">
-            <div class="top flex flex-col gap-2">
-              <h1
-                class="font-medium text-base text-slate-700 overflow-hidden text-ellipsis whitespace-nowrap"
-              >
-                Елка с эффиктом снега доставка бесплатная
-              </h1>
-              <span class="flex justify-between">
-                <span class="w-[77%] flex items-center gap-2"
-                  ><i class="pi pi-money-bill text-gray-400"></i>
-                  <h2 class="font-semibold text-xl">230 000 сум</h2></span
-                >
-                <span class="bg-green-400 w-1/3 text-center rounded-2xl"
-                  ><h1 class="text-white tracking-wider">Active</h1></span
-                >
-              </span>
-            </div>
-            <div class="center flex flex-col">
-              <span class="flex items-center gap-2">
-                <i class="pi pi-map-marker text-gray-400"></i>
-                <span class="text-sm">Ташкент, Учтепинский район</span>
-              </span>
-              <span class="flex justify-between"
-                ><div>
-                  <span class="flex items-center justify-center gap-2">
-                    <i class="pi pi-clock text-gray-400"></i>
-                    <span>Сегодня 10:23</span>
-                  </span>
-                </div>
-                <div class="text-end px-2 bg-white rounded-lg">
-                  <h1>ID:123456</h1>
-                </div></span
-              >
-            </div>
-          </div>
+          <RouterLink to="/">
+            <Button
+              class=""
+              icon="pi pi-arrow-right"
+              size="small"
+              severity="secondary"
+            ></Button>
+          </RouterLink>
         </div>
       </div>
     </div>
+      </div>
   </div>
 </template>
 
