5003c29f9581c897d62d668450a1333c3f86e133
JavohirBozorboyev <javohirbozorboyev2002@gmail.com>
2024-11-06 19:04:53 -0800

    chatid page ux update

--- src/views/ChatIdView.vue
+++ src/views/ChatIdView.vue
@@ -9,9 +9,9 @@ import Avatar from "primevue/avatar";
 </script>
 
 <template>
-  <section class="rounded-md md:flex justify-between flex-col bg-slate-100 md:p-2">
+  <section class="">
     <nav
-      class="bg-white flex items-center gap-2 -m-2 md:m-0 md:rounded-md px-2 overflow-hidden border-b md:border-b-0 z-10 relative"
+      class="bg-white md:bg-slate-100 shadow md:shadow-none w-full fixed top-0 left-0 right-0 z-[1000] md:static flex items-center gap-2 md:rounded-md px-2 overflow-hidden"
     >
       <RouterLink to="/chat" class="block p-2 rounded-md">
         <i class="pi pi-arrow-left text-slate-800"></i>
@@ -26,17 +26,25 @@ import Avatar from "primevue/avatar";
         <h1 class="text-slate-800 text-lg">Coder Fisher</h1>
       </div>
     </nav>
-    <article class="min-h-[90vh] md:min-h-[70vh] mt-4 rounded-md"></article>
     <article
-      class="fixed md:static w-full bottom-0 left-0 p-2 z-[1000] bg-white md:rounded-md"
+      class="min-h-[90vh] md:min-h-[70vh] my-2 rounded-md bg-slate-100"
+    ></article>
+    <article
+      class="fixed md:static w-full bottom-0 left-0 p-2 z-[1000] bg-white md:bg-slate-100 md:rounded-md"
     >
       <div class="container flex gap-2 mx-auto">
-        <InputText placeholder="Хабар" class="w-full" />
+        <InputText
+          placeholder="Хабар"
+          class="w-full rounded-lg"
+          size="large"
+          style="border-radius: 40px"
+        />
         <Button
           size="small"
           severity="contrast"
           icon="pi pi-send"
           aria-label="Save"
+          style="border-radius: 40px; width: 55px; font-size: 24px"
         />
       </div>
     </article>
